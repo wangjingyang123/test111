37410b9b877d43be2cef81c781ca8ef7e522c9dc
zl <zl@zldeMacBook-Pro.local>
2017-10-19 16:10:39 +0800

    123

--- test/AppDelegate.m
+++ test/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     //123
+    NSLog(@"123");
     return YES;
 }
 
